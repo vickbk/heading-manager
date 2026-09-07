25573c46c36210912c67cba1278e64c7799cfe37
vickbk <vickbake8@gmail.com>
2026-09-07 09:32:18 +0200

    chore: add @vickbk/ci-tools dependency and update post-vitest-coverage scripts

--- scripts/bin/post-vitest-coverage.ts
+++ scripts/bin/post-vitest-coverage.ts
@@ -1,5 +1,5 @@
-import { runTask } from "@/scripts/core/errors";
-import { postCoverageComment } from "@/scripts/features/vitest";
+import { runTask } from "@vickbk/ci-tools/core";
+import { postCoverageComment } from "@vickbk/ci-tools/vitest";
 
 await runTask(
   "post-vitest-coverage",
